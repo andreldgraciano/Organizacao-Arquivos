8993891249b53fb1e163f6da983629a2222d0985
andreldgraciano <dedasgdias@gmail.com>
2024-10-10 18:42:44 -0300

    scripts global

---
+++ assets/js/global/script-global.js
@@ -0,0 +1,16 @@
+const globalFunctions = {
+    initNavbar() {
+        console.log("Navbar inicializada");
+    },
+    initFooter() {
+        console.log("Footer inicializado");
+    },
+
+    siteName: "Meu Site",
+    settings: {
+        theme: "dark",
+        layout: "grid"
+    }
+};
+
+export default globalFunctions;
---
+++ assets/js/pages/example/script-example.js
@@ -0,0 +1,6 @@
+import globalFunctions from '../../global/script-global.js';
+
+globalFunctions.initNavbar();
+globalFunctions.initFooter();
+console.log(globalFunctions.siteName);
+console.log(globalFunctions.settings.theme);
